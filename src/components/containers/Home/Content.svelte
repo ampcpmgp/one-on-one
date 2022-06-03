3dbe4aef41234b4e501b5036a48a3bbde66aa416
ampcpmgp <ampcpmgp@gmail.com>
2022-06-03 12:10:53 +0900

    chore: implement generate id button

--- src/components/containers/Home/Content.svelte
+++ src/components/containers/Home/Content.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { nanoid } from "nanoid";
   import { AYAME_WS_URL } from "./../../../constants/ayame.js";
   import { id, tmpId } from "../../../states/!common/room";
   import * as Ayame from "@open-ayame/ayame-web-sdk";
@@ -47,12 +48,37 @@
     });
     localVideo.srcObject = mediaStream;
   }
+
+  function handleRoomIdGenerateButtonClick() {
+    $id = nanoid();
+
+    var type = "text/plain";
+    var blob = new Blob([$id], { type });
+    var data = [new window.ClipboardItem({ [type]: blob })];
+
+    navigator.clipboard.write(data).then(
+      function () {
+        // alert("ルームIDをコピーしました!");
+      },
+      function () {
+        /* failure */
+      }
+    );
+  }
 </script>
 
-<input type="text" bind:value={$id} placeholder="ルームIDを入力してください" />
-<button on:click={() => enterRoom($id)}>入室する</button>
-<button>ルーム作成</button>
-<button on:click={() => enterRoom($tmpId)}>カメラ・マイクチェック</button>
+{#if currentRoomId === ""}
+  <input
+    type="text"
+    bind:value={$id}
+    placeholder="ルームIDを入力してください"
+  />
+  <button on:click={() => enterRoom($id)}>入室する</button>
+  <button on:click={handleRoomIdGenerateButtonClick}>ルームID生成</button>
+  <button on:click={() => enterRoom($tmpId)}>カメラ・マイクチェック</button>
+{:else}
+  <button on:click={() => exitRoom()}>退室する</button>
+{/if}
 
 {#if currentRoomId === $tmpId}
   <!-- svelte-ignore a11y-media-has-caption -->
@@ -64,8 +90,6 @@
 {:else}
   <HowToUse />
 {/if}
-
-<button on:click={() => exitRoom()}>退室する</button>
 
 <style>
   input[type="text"] {
