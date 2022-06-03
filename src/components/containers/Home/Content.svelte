44d977671d8bddfed90df1be25c8b41fd700b563
ampcpmgp <ampcpmgp@gmail.com>
2022-06-03 23:13:13 +0900

    chore: move room entry function

--- src/components/containers/Home/Content.svelte
+++ src/components/containers/Home/Content.svelte
@@ -3,28 +3,10 @@
   import RoomEntry from "./RoomEntry.svelte";
   import DeviceCheck from "./DeviceCheck.svelte";
   import Technology from "./Technology.svelte";
-  import { nanoid } from "nanoid";
-  import { formId, tmpId, currentId } from "../../../states/!common/room";
+  import { tmpId, currentId } from "../../../states/!common/room";
   import HowToUse from "./HowToUse.svelte";
   import Overview from "./Overview.svelte";
   import { srcObject } from "../../../actions/video";
-
-  function handleRoomIdGenerateButtonClick() {
-    $formId = nanoid();
-
-    var type = "text/plain";
-    var blob = new Blob([$formId], { type });
-    var data = [new window.ClipboardItem({ [type]: blob })];
-
-    navigator.clipboard.write(data).then(
-      function () {
-        // alert("ルームIDをコピーしました!");
-      },
-      function () {
-        /* failure */
-      }
-    );
-  }
 </script>
 
 <Overview />
@@ -33,17 +15,7 @@
 <RoomEntry />
 <Technology />
 
-{#if $currentId === ""}
-  <input
-    type="text"
-    bind:value={$formId}
-    placeholder="ルームIDを入力してください"
-  />
-  <button on:click={() => ($currentId = $formId)}>入室する</button>
-  <button on:click={handleRoomIdGenerateButtonClick}>ルームID生成</button>
-{:else}
-  <button on:click={() => ($currentId = "")}>退室する</button>
-{/if}
+<button on:click={() => ($currentId = "")}>退室する</button>
 
 {#if $currentId === $tmpId}
   <!-- svelte-ignore a11y-media-has-caption -->
@@ -53,9 +25,3 @@
   <!-- svelte-ignore a11y-media-has-caption -->
   <video use:srcObject={$remoteStream} autoplay playsinline />
 {/if}
-
-<style>
-  input[type="text"] {
-    width: 280px;
-  }
-</style>
--- src/components/containers/Home/RoomEntry.svelte
+++ src/components/containers/Home/RoomEntry.svelte
@@ -1,1 +1,37 @@
-<h2>ルーム入室</h2>
+<script>
+  import { nanoid } from "nanoid";
+  import { formId, currentId } from "../../../states/!common/room";
+
+  function handleRoomIdGenerateButtonClick() {
+    $formId = nanoid();
+
+    var type = "text/plain";
+    var blob = new Blob([$formId], { type });
+    var data = [new window.ClipboardItem({ [type]: blob })];
+
+    navigator.clipboard.write(data).then(
+      function () {
+        // alert("ルームIDをコピーしました!");
+      },
+      function () {
+        /* failure */
+      }
+    );
+  }
+</script>
+
+<h2>ルーム</h2>
+
+<input
+  type="text"
+  bind:value={$formId}
+  placeholder="ルームIDを入力してください"
+/>
+<button on:click={() => ($currentId = $formId)}>入室する</button>
+<button on:click={handleRoomIdGenerateButtonClick}>ルームID生成</button>
+
+<style>
+  input[type="text"] {
+    width: 280px;
+  }
+</style>
