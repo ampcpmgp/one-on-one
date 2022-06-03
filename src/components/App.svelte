7f9b1ff565550494695dd530b754a78a3925f6bb
ampcpmgp <ampcpmgp@gmail.com>
2022-06-03 23:31:09 +0900

    chore: move Header

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,6 @@
 <script>
   import Router from "svelte-spa-router";
-  import Header from "./parts/Header/Header.svelte";
   import routes from "./routes";
 </script>
 
-<Header />
 <Router {routes} />
