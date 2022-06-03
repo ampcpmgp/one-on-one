3cfc424a3c937ebbb7b32caaa7ade985cb028182
ampcpmgp <ampcpmgp@gmail.com>
2022-06-03 23:33:09 +0900

    chore: add pages

---
+++ src/components/routes/Checker.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Content from "../containers/Home/Content.svelte";
+  import Header from "../parts/Header/Header.svelte";
+</script>
+
+<Header />
+<Content />
--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -1,7 +1,11 @@
+import CheckerSvelte from "./Checker.svelte";
 import Home from "./Home.svelte";
 import NotFound from "./NotFound.svelte";
+import RoomSvelte from "./Room.svelte";
 
 export default {
   "/": Home,
+  "/checker": CheckerSvelte,
+  "/room/:id": RoomSvelte,
   "*": NotFound,
 };
